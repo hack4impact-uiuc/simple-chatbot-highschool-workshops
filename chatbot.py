c8a674a18020f22af3e7db9e134a8cd436d72eb3
Timothy Ko <timothy.l.ko@gmail.com>
2019-02-27 17:49:13 -0600

    Random choices

--- chatbot.py
+++ chatbot.py
@@ -1,18 +1,19 @@
 import random
 
 greetings = ["hola", "hello", "hi", "Hi", "hey!", "hey"]
-random_greeting = random.choice(greetings)
+greeting_responses = ["Hi, I'm a robot", "What's up, I'm bob"]
 
 question = ["How are you?", "How are you doing?"]
-responses = ["Okay", "I'm fine"]
-random_response = random.choice(responses)
+question_responses = ["Okay", "I'm fine"]
 
 
 while True:
     userInput = input("Type something! >>> ")
     if userInput in greetings:
+        random_greeting = random.choice(greeting_responses)
         print(random_greeting)
     elif userInput in question:
+        random_response = random.choice(question_responses)
         print(random_response)
     else:
         print("I did not understand what you said")
